ddfaa5fa9bb8f905ddd9e0a4d72fe9fd4a55f592
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-09-14 20:39:46 +0300

    zephyr: Enable building ussl_mbedtls module.
    Using Zephyr's builtin mbedtls.

--- zephyr/src/zephyr_start.c
+++ zephyr/src/zephyr_start.c
@@ -35,3 +35,12 @@ void main(void) {
     zephyr_getchar_init();
     real_main();
 }
+
+// This gets pulled in when mbedTLS is enabled in Zephyr config.
+// It doesn't seem to be used by mbedTLS directly directly, but
+// rather pulled in for some other libc dependency. All in all,
+// this doesn't seem to be called, so included to avoid link
+// error.
+int gettimeofday(struct timeval *tv, struct timezone *tz) {
+    printf("! dummy gettimeofday called\n");
+}
