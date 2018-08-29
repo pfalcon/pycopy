4562399df08ef3a1df5684816ff534209f8b72e0
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-08-29 20:45:47 +0300

    tests/extmod/uctypes_ptr_le.py: Test PTR assignment.

--- tests/extmod/uctypes_ptr_le.py
+++ tests/extmod/uctypes_ptr_le.py
@@ -18,7 +18,7 @@ desc = {
 bytes = b"01"
 
 addr = uctypes.addressof(bytes)
-buf = addr.to_bytes(uctypes.sizeof(desc), "little")
+buf = addr.to_bytes(uctypes.sizeof(desc, uctypes.LITTLE_ENDIAN), "little")
 
 S = uctypes.struct(uctypes.addressof(buf), desc, uctypes.LITTLE_ENDIAN)
 
@@ -35,3 +35,16 @@ print(S.ptr2[0].b, S.ptr2[1].b)
 print (S.ptr2[0].b, S.ptr2[1].b)
 print(hex(S.ptr16[0]))
 assert (S.ptr2[0].b, S.ptr2[1].b) == (48, 49)
+
+# Check that can assign address to a pointer
+S.ptr = uctypes.addressof(b"23")
+print(S.ptr[0])
+assert S.ptr[0] == ord("2")
+print(S.ptr[1])
+assert S.ptr[1] == ord("3")
+
+# Check that can assign one pointer to another
+buf2 = bytearray(uctypes.sizeof(desc, uctypes.LITTLE_ENDIAN))
+S2 = uctypes.struct(uctypes.addressof(buf2), desc, uctypes.LITTLE_ENDIAN)
+S2.ptr = S.ptr
+print(hex(S2.ptr16[0]))
