71f3f9304e8dd262d2dae129500370d7555680a7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-09-16 23:45:20 +0300

    py/modsys: Implement sys.intern() function.
    Allows to intern arbitrary-sized string, compliant with CPython. Added
    mostly for completeness/experimentation/fine-tuning, not expected to be
    enabled by default in any port.

--- py/modsys.c
+++ py/modsys.c
@@ -146,6 +146,21 @@ STATIC mp_obj_t mp_sys_getsizeof(mp_obj_t obj) {
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_sys_getsizeof_obj, mp_sys_getsizeof);
 #endif
 
+#if MICROPY_PY_SYS_INTERN
+STATIC mp_obj_t mp_sys_intern(mp_obj_t str_in) {
+    if (MP_OBJ_IS_QSTR(str_in)) {
+        return str_in;
+    }
+    if (!MP_OBJ_IS_TYPE(str_in, &mp_type_str)) {
+        mp_raise_TypeError(NULL);
+    }
+
+    GET_STR_DATA_LEN(str_in, str_data, str_len);
+    return mp_obj_new_str_via_qstr((const char*)str_data, str_len);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_sys_intern_obj, mp_sys_intern);
+#endif
+
 STATIC const mp_rom_map_elem_t mp_module_sys_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_sys) },
 
@@ -194,6 +209,9 @@ STATIC const mp_rom_map_elem_t mp_module_sys_globals_table[] = {
     #endif
     #if MICROPY_PY_SYS_GETSIZEOF
     { MP_ROM_QSTR(MP_QSTR_getsizeof), MP_ROM_PTR(&mp_sys_getsizeof_obj) },
+    #endif
+    #if MICROPY_PY_SYS_INTERN
+    { MP_ROM_QSTR(MP_QSTR_intern), MP_ROM_PTR(&mp_sys_intern_obj) },
     #endif
 
     /*
