542f7719c1e1b07352fe6a6ebefee9d96b245a82
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-10-13 19:29:01 +0300

    tests/sys_getsizeof*: Add more types (int, float, str, tuple, set).

--- tests/basics/sys_getsizeof.py
+++ tests/basics/sys_getsizeof.py
@@ -1,4 +1,5 @@
 # test sys.getsizeof() function
+# note - float test is in ../float/
 
 import sys
 try:
@@ -7,12 +8,21 @@ except AttributeError:
     print('SKIP')
     raise SystemExit
 
+print(sys.getsizeof(1) >= 2)
+print(sys.getsizeof("") >= 2)
+print(sys.getsizeof((1, 2)) >= 2)
 print(sys.getsizeof([1, 2]) >= 2)
 print(sys.getsizeof({1: 2}) >= 2)
 
 class A:
     pass
 print(sys.getsizeof(A()) > 0)
+
+
+try:
+    assert sys.getsizeof(set()) >= 2
+except NameError:
+    pass
 
 # Only test deque if we have it
 try:
