0cfa89c0f5873d8ab0b9c34acd95f8f7a23e46be
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-12-12 01:18:45 +0300

    tests/class1_slots: Basic tests for __slots__.
    Change-Id: I35858f2b21b8744d3f62bc8858100b9634a1f965

---
+++ tests/basics/class_slots.py
@@ -0,0 +1,31 @@
+# basic class with __slots__
+
+def go():
+    class C:
+
+        __slots__ = ("value",)
+
+        def f():
+            print(1)
+
+        def g(self):
+            print(2)
+
+        def set(self, value):
+            self.value = value
+
+        def print(self):
+            print(self.value)
+
+    C.f()
+    C()
+    C().g()
+
+    o = C()
+    o.set(3)
+    o.print()
+
+    C.set(o, 4)
+    C.print(o)
+
+go()
