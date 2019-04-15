3d3bd64006c7ca40172f33787a80ffa4d394f609
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-04-15 21:05:45 +0300

    tests/utimeq_handle: Not rely on order of processing of same-time entries.
    Don't assume that entries with the same value of time will be popped in
    the same order as pushed (aka that the module supports stable order for
    push/pop).
    
    This is in preparation of making stable order support optional (and
    allowing to optimize memory usage by disabling it).
    
    Change-Id: I60fe87f3f7fce6f1071dbdcce1812e1075dd6632

--- tests/extmod/utimeq_handle.py
+++ tests/extmod/utimeq_handle.py
@@ -7,9 +7,9 @@ except ImportError:
 h = utimeq(4)
 
 assert h.push(0, 1, 2) == 0
-assert h.push(0, 1, 2) == 1
-assert h.push(0, 1, 2) == 2
-assert h.push(0, 1, 2) == 3
+assert h.push(1, 1, 2) == 1
+assert h.push(2, 1, 2) == 2
+assert h.push(3, 1, 2) == 3
 
 try:
     h.push(0, 1, 2)
@@ -18,18 +18,18 @@ except IndexError:
     pass
 
 h.pop([0, 0, 0])
-assert h.push(0, 1, 2) == 0
+assert h.push(4, 1, 2) == 0
 
 h.pop([0, 0, 0])
-assert h.push(0, 1, 2) == 1
+assert h.push(5, 1, 2) == 1
 
 h.pop([0, 0, 0])
-assert h.push(0, 1, 2) == 2
+assert h.push(6, 1, 2) == 2
 
 h.pop([0, 0, 0])
-assert h.push(0, 1, 2) == 3
+assert h.push(7, 1, 2) == 3
 
 h.pop([0, 0, 0])
-assert h.push(0, 1, 2) == 0
+assert h.push(8, 1, 2) == 0
 
 print("OK")
