4904a9d40118ef12d65f81aaa9e5fcd37f402dd1
Ivan Ignatenko <31288825+COOLIRON2311@users.noreply.github.com>
2019-04-30 14:40:12 +0300

    Suggestion to implement gcd in math module

--- lib/libm/gcd.c
+++ lib/libm/gcd.c
@@ -2,8 +2,6 @@
  * This is a suggestion of implementing rather optimal Euclidean algorithm
  * of finding Greatest Common Divider.
  *
- * The the first one is iterative:
- *
  * int gcd(int a, int b) {
  *   int t;
  *   while (b != 0) {
@@ -12,24 +10,5 @@
  *     a = t;
  *   }
  *   return a;
- * }
- * 
- * The second one is a version with loop-unrolling:
- *
- * int gcd(int a, int b)  
- * {  
- *   while(true)  
- *   {  
- *     a = a % b;  
- *     if(a == 0)  
- *     {  
- *       return b;  
- *     }  
- *     b = b % a;  
- *     if(b == 0)  
- *     {  
- *       return a;  
- *     }  
- *   }  
  * }
  */
