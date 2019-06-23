8a849cde4e770b95ae2f3c1a7eb5a2f5bffa5bed
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-06-23 16:52:01 +0300

    py/modio: Add BufferedReader class implementation.
    To complement previously implemented BufferedWriter. Buffered streams are
    absolutely a must for performance. BufferedReader follows about the same
    buffer management policy as BufferedWriter - it reads from the underlying
    stream in chunks of buffer size, size could work directly with block
    devices.
    
    Change-Id: I8ba13084520b555a1df0b087329b8b6f70646804

--- py/modio.c
+++ py/modio.c
@@ -105,6 +105,77 @@ STATIC const mp_obj_type_t mp_type_iobase = {
 
 #endif // MICROPY_PY_IO_IOBASE
 
+#if MICROPY_PY_IO_BUFFEREDREADER
+typedef struct _mp_obj_bufreader_t {
+    mp_obj_base_t base;
+    mp_obj_t stream;
+    size_t alloc;
+    size_t len;
+    size_t pos;
+    byte buf[0];
+} mp_obj_bufreader_t;
+
+STATIC mp_obj_t bufreader_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    mp_arg_check_num(n_args, n_kw, 2, 2, false);
+    size_t alloc = mp_obj_get_int(args[1]);
+    mp_obj_bufreader_t *o = m_new_obj_var(mp_obj_bufreader_t, byte, alloc);
+    o->base.type = type;
+    o->stream = args[0];
+    o->alloc = alloc;
+    o->len = 0;
+    o->pos = 0;
+    return o;
+}
+
+STATIC mp_uint_t bufreader_read(mp_obj_t self_in, void *buf, mp_uint_t size, int *errcode) {
+    mp_obj_bufreader_t *self = MP_OBJ_TO_PTR(self_in);
+
+    mp_uint_t ret_sz = 0;
+
+    while (size != 0) {
+        if (self->len == 0) {
+            mp_uint_t in_sz = mp_stream_read_exactly(self->stream, self->buf, self->alloc, errcode);
+            if (in_sz == 0 || in_sz == MP_STREAM_ERROR) {
+                if (ret_sz != 0) {
+                    return ret_sz;
+                }
+                return in_sz;
+            }
+
+            self->pos = 0;
+            self->len = in_sz;
+        }
+
+        mp_uint_t buf_sz = MIN(size, self->len);
+        memcpy(buf, self->buf + self->pos, buf_sz);
+        self->pos += buf_sz;
+        self->len -= buf_sz;
+        size -= buf_sz;
+        ret_sz += buf_sz;
+        buf = (byte*)buf + buf_sz;
+    }
+
+    return ret_sz;
+}
+
+STATIC const mp_rom_map_elem_t bufreader_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&mp_stream_read_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(bufreader_locals_dict, bufreader_locals_dict_table);
+
+STATIC const mp_stream_p_t bufreader_stream_p = {
+    .read = bufreader_read,
+};
+
+STATIC const mp_obj_type_t bufreader_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_BufferedReader,
+    .make_new = bufreader_make_new,
+    .protocol = &bufreader_stream_p,
+    .locals_dict = (mp_obj_dict_t*)&bufreader_locals_dict,
+};
+#endif // MICROPY_PY_IO_BUFFEREDREADER
+
 #if MICROPY_PY_IO_BUFFEREDWRITER
 typedef struct _mp_obj_bufwriter_t {
     mp_obj_base_t base;
@@ -271,6 +342,9 @@ STATIC const mp_rom_map_elem_t mp_module_io_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_StringIO), MP_ROM_PTR(&mp_type_stringio) },
     #if MICROPY_PY_IO_BYTESIO
     { MP_ROM_QSTR(MP_QSTR_BytesIO), MP_ROM_PTR(&mp_type_bytesio) },
+    #endif
+    #if MICROPY_PY_IO_BUFFEREDREADER
+    { MP_ROM_QSTR(MP_QSTR_BufferedReader), MP_ROM_PTR(&bufreader_type) },
     #endif
     #if MICROPY_PY_IO_BUFFEREDWRITER
     { MP_ROM_QSTR(MP_QSTR_BufferedWriter), MP_ROM_PTR(&bufwriter_type) },
