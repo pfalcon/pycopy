1a173b00a7f6309bb3a9636e43865f7e37e4398b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-10 19:16:36 +0300

    tools/gen-cpydiff: Use "pycopy" executable.
    Change-Id: If978f8e28bd80f5687a6a0c4b3e83d804faf6fba

--- tools/gen-cpydiff.py
+++ tools/gen-cpydiff.py
@@ -39,10 +39,10 @@ from collections import namedtuple
 # to the correct executable.
 if os.name == 'nt':
     CPYTHON3 = os.getenv('MICROPY_CPYTHON3', 'python3.exe')
-    MICROPYTHON = os.getenv('MICROPY_MICROPYTHON', '../ports/windows/micropython.exe')
+    MICROPYTHON = os.getenv('MICROPY_MICROPYTHON', '../ports/windows/pycopy.exe')
 else:
     CPYTHON3 = os.getenv('MICROPY_CPYTHON3', 'python3')
-    MICROPYTHON = os.getenv('MICROPY_MICROPYTHON', '../ports/unix/micropython')
+    MICROPYTHON = os.getenv('MICROPY_MICROPYTHON', '../ports/unix/pycopy')
 
 TESTPATH = '../tests/cpydiff/'
 DOCPATH = '../docs/genrst/'
