76555d438d35b1af86aeb6f5204a132955917c8f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-17 09:54:50 +0300

    py/strict_mode: Beginnings of "strict mode" for runtime semantics.
    In strict mode, adding/deleting attributes to module/class namespaces
    (including module's own global namespace) at "run-time" is not allowed.
    Instead, all attributes should be declared at "import-time". In general,
    at import time, arbitrary dynamic processing and meta-programming is
    allowed, but it should lead to a static namespace snapshort at the
    run-time.
    
    To clearly separate import-time from run-time phases, a new convention
    of the __main__() function is introduced: the main module of an
    applicaiton (one which is passed to the interpreter on the command line)
    should contain a function named __main__(). After importing entire module
    set (during which arbitrary namespace processing and metaprogramming
    is allowed), the interpreter will switch to run-time module and
    execute the __main__ function.
    
    Strict mode activation is implemented for unix port, using "-X strict"
    option.
    
    This commit implements neginnging of strict mode semantics, it's going
    to be extended in upcoming commits.
    
    Change-Id: I598dde1bb887a42a6917ab699f59f54001f3cf56
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ py/strict_mode.c
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the Pycopy project, https://github.com/pfalcon/pycopy
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Paul Sokolovsky
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdbool.h>
+
+#include "py/runtime.h"
+#include "py/strict_mode.h"
+
+bool mp_strict_mode;
+bool mp_strict_runtime;
+
+NORETURN void mp_strict_mode_error(void) {
+    mp_raise_msg(&mp_type_RuntimeError,
+        MP_ERROR_TEXT("strict mode: can't create/del namespace attrs at runtime"));
+}
+
+bool mp_handle_store_ns_strict(mp_map_t *map, mp_obj_t attr, mp_obj_t val) {
+    if (mp_strict_runtime) {
+        mp_map_elem_t *elem = mp_map_lookup(map, attr, MP_MAP_LOOKUP);
+        if (elem == NULL) {
+            mp_strict_mode_error();
+        }
+        elem->value = val;
+        return true;
+    }
+
+    return false;
+}
