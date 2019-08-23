fda6809065953f3dc1df3c0884861d6af6993aad
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-08-23 10:31:53 +0300

    py/strict_mode.h: Const qstr key helpers.
    Introduce notion of "const qstr keys" for namespace (qstr-only) maps.
    Such keys have the LSB set. Normally, that would make them look like
    int's, but given that we know that we talk about qstr-only maps, and
    thus keys, this LSB will allow us to disambiguate "const" slots in
    such maps from "non-const".
    
    Change-Id: If36dc3f4d1215116d81a8670c54893132fa98c6c

--- py/strict_mode.h
+++ py/strict_mode.h
@@ -33,3 +33,38 @@ extern bool mp_strict_runtime;
 NORETURN void mp_strict_mode_error(void);
 bool mp_handle_store_ns_strict(mp_map_t *map, mp_obj_t attr, mp_obj_t val);
 void mp_strict_update_main_name(void);
+
+
+#if MICROPY_OBJ_REPR == MICROPY_OBJ_REPR_A
+
+#define MP_MAP_QSTR_KEY_UNCONST(key) ((mp_obj_t)((mp_int_t)(key) & ~1))
+#define MP_MAP_QSTR_KEY_CONST(key) ((mp_obj_t)((mp_int_t)(key) | 1))
+#define MP_MAP_QSTR_KEY_IS_CONST(key) ((mp_int_t)(key) & 1)
+#define MP_MAP_GET_KEY_MASK(map) ((map)->all_keys_are_qstrs ? (mp_uint_t) ~1 : (mp_uint_t) ~0)
+#define MP_MAP_KEY_APPLY_MASK(key, mask) ((mp_obj_t)((mp_uint_t)(key) & (mask)))
+
+#elif MICROPY_OBJ_REPR == MICROPY_OBJ_REPR_C
+
+#define MP_MAP_QSTR_KEY_UNCONST(key) ((mp_obj_t)((mp_int_t)(key) & ~1))
+#define MP_MAP_QSTR_KEY_CONST(key) ((mp_obj_t)((mp_int_t)(key) | 1))
+#define MP_MAP_QSTR_KEY_IS_CONST(key) ((mp_int_t)(key) & 1)
+#define MP_MAP_GET_KEY_MASK(map) ((map)->all_keys_are_qstrs ? (mp_uint_t) ~1 : (mp_uint_t) ~0)
+#define MP_MAP_KEY_APPLY_MASK(key, mask) ((mp_obj_t)((mp_uint_t)(key) & (mask)))
+
+#elif MICROPY_OBJ_REPR == MICROPY_OBJ_REPR_D
+
+#define MP_MAP_QSTR_KEY_UNCONST(key) ((mp_obj_t)((uint64_t)(key) | 1))
+#define MP_MAP_QSTR_KEY_CONST(key) ((mp_obj_t)((uint64_t)(key) & ~1))
+#define MP_MAP_QSTR_KEY_IS_CONST(key) (((uint64_t)(key) & 1) == 0)
+#define MP_MAP_GET_KEY_MASK(map) ((map)->all_keys_are_qstrs ? (mp_uint_t)1 : (mp_uint_t)0)
+#define MP_MAP_KEY_APPLY_MASK(key, mask) ((mp_obj_t)((mp_uint_t)(key) | (mask)))
+
+#else
+
+#error Constant namespace slots are not supported by this object representation
+
+#endif
+
+// Abstracts away MP_MAP_QSTR_KEY_UNCONST(), in case more flag bits are
+// added in the future.
+#define MP_MAP_NS_KEY(key) MP_MAP_QSTR_KEY_UNCONST(key)
