551f053fe0942ccd3b1e36599a7d83d5d62f9620
Damien George <damien.p.george@gmail.com>
2019-09-02 13:09:44 +1000

    py/binary: Change mp_uint_t to size_t for index, size, align args.
    Reduces code size for nan-box builds, otherwise changes nothing.
    
    pfalcon: Updated for Pycopy codebase.
    Conflicts:
    	py/binary.c
    	py/binary.h
    
    Change-Id: I1045aa8294719e9b23728ea01ef7396343547b39

--- py/binary.h
+++ py/binary.h
@@ -37,14 +37,14 @@
 // Special typecode for BytesIO's internal buffer.
 #define BYTESIO_TYPECODE 2
 
-size_t mp_binary_get_size(char struct_type, char val_type, mp_uint_t *palign);
-mp_obj_t mp_binary_get_val_array(char typecode, void *p, mp_uint_t index);
-void mp_binary_set_val_array(char typecode, void *p, mp_uint_t index, mp_obj_t val_in);
-void mp_binary_set_val_array_from_int(char typecode, void *p, mp_uint_t index, mp_int_t val);
+size_t mp_binary_get_size(char struct_type, char val_type, size_t *palign);
+mp_obj_t mp_binary_get_val_array(char typecode, void *p, size_t index);
+void mp_binary_set_val_array(char typecode, void *p, size_t index, mp_obj_t val_in);
+void mp_binary_set_val_array_from_int(char typecode, void *p, size_t index, mp_int_t val);
 mp_obj_t mp_binary_get_val(char struct_type, char val_type, const byte *buf, size_t *offset);
 void mp_binary_set_val(char struct_type, char val_type, mp_obj_t val_in, byte *buf, size_t *offset);
-long long mp_binary_get_int(mp_uint_t size, bool is_signed, bool big_endian, const byte *src);
-void mp_binary_set_int(mp_uint_t val_sz, bool big_endian, byte *dest, mp_uint_t val);
+long long mp_binary_get_int(size_t size, bool is_signed, bool big_endian, const byte *src);
+void mp_binary_set_int(size_t val_sz, bool big_endian, byte *dest, mp_uint_t val);
 
 // Higher-level reusable functions from modstruct.c
 char mp_struct_get_fmt_type(const char **fmt);
