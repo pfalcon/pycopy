e4fd3324b1a8ace7571aa6955069e601aeb1c0a6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-02-08 11:51:06 +0700

    all: Version 3.0.3.
    Change-Id: I669ecc932b3073d736a038d13e255036500a1c86
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -30,7 +30,7 @@
 // Current version of Pycopy
 #define MICROPY_VERSION_MAJOR 3
 #define MICROPY_VERSION_MINOR 0
-#define MICROPY_VERSION_MICRO 2
+#define MICROPY_VERSION_MICRO 3
 
 // Combined version as a 32-bit number for convenience
 #define MICROPY_VERSION ( \
