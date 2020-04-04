2f13b7b1d77ee10df2b94046cc69bec04ea48947
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-04-04 16:55:43 +0300

    ests/extmod/uctypes_*: Add "# fmt: off" markers.
    For formatting tools to leave data definitions alone.
    
    Change-Id: I6ae6279466e3e7cfd0302d7204a326e0db40ea90
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- tests/extmod/uctypes_union.py
+++ tests/extmod/uctypes_union.py
@@ -10,11 +10,13 @@ OFFSET_MASK = (1 << OFFSET_BITS) - 1
 
 o = OrderedDict
 
+# fmt: off
 desc = o((
     ("u0", uctypes.UINT8),
     ("u1", uctypes.PREV_OFFSET | uctypes.UINT16),
     ("u2", uctypes.PREV_OFFSET | uctypes.UINT32),
 ))
+# fmt: on
 
 sizeof = uctypes.calc_offsets(desc, uctypes.LITTLE_ENDIAN)
 print(sizeof)
@@ -26,12 +28,14 @@ offsets = [(x[0] if isinstance(x, tuple) else x) & OFFSET_MASK for x in desc.val
 print(offsets)
 
 
+# fmt: off
 desc = o((
     ("u0", uctypes.UINT8),
     ("u1", uctypes.PREV_OFFSET | uctypes.UINT16),
     ("u2", uctypes.PREV_OFFSET | uctypes.UINT32),
     ("f1", uctypes.UINT16)
 ))
+# fmt: on
 
 sizeof = uctypes.calc_offsets(desc, uctypes.LITTLE_ENDIAN)
 print(sizeof)
