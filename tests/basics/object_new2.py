daf221dcab7f1856d089922a6645c70f3e2ecc98
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-04-23 02:39:15 +0300

    tests/basics/object_new2: Test for various ways to call object.__new__().
    Change-Id: I42a487014455d3d49e3e4f795ef2192d9a35756e
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ tests/basics/object_new2.py
@@ -0,0 +1,66 @@
+# Various ways to call object.__new__
+try:
+    # If we don't expose object.__new__ (small ports), there's
+    # nothing to test.
+    object.__new__
+except AttributeError:
+    print("SKIP")
+    raise SystemExit
+
+
+try:
+    object.__new__(int)
+except TypeError:
+    print("TypeError1")
+
+try:
+    object.__new__(int, 1)
+except TypeError:
+    print("TypeError2")
+
+o = object()
+print(type(o))
+
+try:
+    object(1)
+except TypeError:
+    print("TypeError3")
+
+class A: pass
+
+o2 = object.__new__(A)
+print(type(o2).__name__)
+
+try:
+    object.__new__(A, 1)
+except TypeError:
+    print("TypeError4")
+
+
+class B:
+    field = 1
+
+o3 = object.__new__(B)
+assert "field" in dir(o3)
+
+
+class C:
+    def __init__(self, a, b):
+        1/0
+        print(a, b)
+
+# CPython allows this, silently ignores extra params in this case
+# (i.e. when __init__ is defined).
+#o4 = object.__new__(C, 1, 2, 3)
+#print(o4, dir(o4))
+
+
+class D:
+    pass
+    def __new__(self, a, b):
+        print(a, b)
+
+try:
+    o5 = object.__new__(D, 1, 2)
+except TypeError:
+    print("TypeError5")
