d92adf63b4fa30b91545afdcd8a0d2a10d96a3a9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-05-09 00:31:15 +0300

    tests/cpydiff: Update for Pycopy.
    Change-Id: I13f794c27b64e40cb1b7d70c636bac0e648a38e5
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- tests/cpydiff/core_import_split_ns_pkgs.py
+++ tests/cpydiff/core_import_split_ns_pkgs.py
@@ -1,8 +1,8 @@
 """
 categories: Core,import
-description: MicroPython does't support namespace packages split across filesystem.
-cause: MicroPython's import system is highly optimized for simplicity, minimal memory usage, and minimal filesystem search overhead.
-workaround: Don't install modules belonging to the same namespace package in different directories. For MicroPython, it's recommended to have at most 3-component module search paths: for your current application, per-user (writable), system-wide (non-writable).
+description: Pycopy does't support namespace packages split across filesystem.
+cause: Pycopy's import system is highly optimized for simplicity, minimal memory usage, and minimal filesystem search overhead.
+workaround: Don't install modules belonging to the same namespace package in different directories. For Pycopy, it's recommended to have at most 3-component module search paths: for your current application, per-user (writable), system-wide (non-writable).
 """
 import sys
 
