c318ff51b9beec62e0626f61a34e7302556b4d56
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-09-19 09:47:21 +0300

    tests/run-multitests.py: Switch to PYCOPYPATH envvar.
    Change-Id: I5878f282491934e63858f387d52ab0c2474f8635
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- tests/run-multitests.py
+++ tests/run-multitests.py
@@ -404,7 +404,7 @@ def main():
     cmd_args = cmd_parser.parse_args()
 
     # clear search path to make sure tests use only builtin modules and those in extmod
-    os.environ["MICROPYPATH"] = os.pathsep + "../extmod"
+    os.environ["PYCOPYPATH"] = os.pathsep + "../extmod"
 
     test_files = prepare_test_file_list(cmd_args.files)
     max_instances = max(t[1] for t in test_files)
