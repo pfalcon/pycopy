021141528083c845926aa2fc5fa4f5e61549f370
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-12-03 21:24:13 +0300

    tests/cpydiff/core_import___all__: No longer applies, remove.
    __all__ handling for "from ... import *" was implemented.
    
    Change-Id: I2315835d1add1491545d15b85f495d08756c36d0
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- tests/cpydiff/core_import___all__.py
+++
@@ -1,9 +0,0 @@
-"""
-categories: Core,import
-description: __all__ attribute of modules/packages isn't processed by Pycopy
-cause: Pycopy employs simplified model, where ``from mod import *`` imports all names, except those starting with an underscore. This handling is compatible with CPython.
-workaround: Don't rely on __all__. For symbols which you don't want to be imported with ``from mod import *``, start their names with ``_``. You can still import such a symbol explicitly with ``from mod import _sym``.
-"""
-from modules.modall import *
-
-print([name for name in dir() if "foo" in name])
