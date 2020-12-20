09dde84592d07855e8e8b5494e7f0bfc09bd88d1
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-12-21 00:40:31 +0300

    tests/extmod/uctypes_byteat: Add testcase for 1-arg uctypes.bytes_at().
    Change-Id: I268292d36cc246250dbb3c2148e599b402a49e6a
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- tests/extmod/uctypes_byteat.py
+++ tests/extmod/uctypes_byteat.py
@@ -8,3 +8,4 @@ data = bytearray(b"01234567")
 
 print(uctypes.bytes_at(uctypes.addressof(data), 4))
 print(uctypes.bytearray_at(uctypes.addressof(data), 4))
+print(uctypes.bytes_at(uctypes.addressof(data) + 2))
