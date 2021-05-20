7348d856d82799f958d69758b5bb6bcb8c73f02b
Damien George <damien@micropython.org>
2021-05-20 10:02:57 +1000

    tests: Make float and framebuf tests skip or run on big-endian archs.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/extmod/framebuf_subclass.py
+++ tests/extmod/framebuf_subclass.py
@@ -6,8 +6,13 @@ raise SystemExit
 # test subclassing framebuf.FrameBuffer
 
 try:
-    import framebuf
+    import framebuf, usys
 except ImportError:
+    print("SKIP")
+    raise SystemExit
+
+# This test and its .exp file is based on a little-endian architecture.
+if usys.byteorder != "little":
     print("SKIP")
     raise SystemExit
 
