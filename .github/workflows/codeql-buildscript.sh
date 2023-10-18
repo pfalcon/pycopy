68c1ab10ade701548851a37c190311a63c2f8dc4
Brian <bayuan@purdue.edu>
2023-10-18 17:05:45 -0400

    Add CodeQL Workflow for Code Security Analysis
    Add CodeQL Workflow for Code Security Analysis
    
    This pull request introduces a CodeQL workflow to enhance the security analysis of our repository. CodeQL is a powerful static analysis tool that helps identify and mitigate security vulnerabilities in our codebase. By integrating this workflow into our GitHub Actions, we can proactively identify and address potential issues before they become security threats.
    
    We added a new CodeQL workflow file (.github/workflows/codeql.yml) that
    - Runs on every pull request (functionality to run on every push to main branches is included as a comment for convenience).
    - Runs daily.
    - Excludes queries with a high false positive rate or low-severity findings.
    - Does not display results for git submodules, focusing only on our own codebase.
    
    Testing:
    To validate the functionality of this workflow, we have run several test scans on the codebase and reviewed the results. The workflow successfully compiles the project, identifies issues, and provides actionable insights while reducing noise by excluding certain queries and third-party code.
    
    Deployment:
    Once this pull request is merged, the CodeQL workflow will be active and automatically run on every push and pull request to the main branch. To view the results of these code scans, please follow these steps:
    1. Under the repository name, click on the Security tab.
    2. In the left sidebar, click Code scanning alerts.
    
    Additional Information:
    - You can further customize the workflow to adapt to your specific needs by modifying the workflow file.
    - For more information on CodeQL and how to interpret its results, refer to the GitHub documentation and the CodeQL documentation (https://codeql.github.com/ and https://codeql.github.com/docs/).
    
    Signed-off-by: Brian <bayuan@purdue.edu>

---
+++ .github/workflows/codeql-buildscript.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# maybe need dependencies?
+#make -C mpy-cross
+#cd ports/unix
+#make submodules
+#make
+
+source tools/ci.sh && ci_unix_minimal_build
+source tools/ci.sh && ci_unix_standard_build
+
+export MAKEOPTS="-j$(nproc)"
+
+make ${MAKEOPTS} -C mpy-cross
+make ${MAKEOPTS} -C ports/stm32 submodules
+git submodule update --init lib/btstack
+make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_F091RC
+make ${MAKEOPTS} -C ports/stm32 BOARD=PYBV11 MICROPY_PY_WIZNET5K=5200 MICROPY_PY_CC3K=1
+make ${MAKEOPTS} -C ports/stm32 BOARD=PYBD_SF2
+make ${MAKEOPTS} -C ports/stm32 BOARD=PYBD_SF6 NANBOX=1 MICROPY_BLUETOOTH_NIMBLE=0 MICROPY_BLUETOOTH_BTSTACK=1
+make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_H743ZI CFLAGS_EXTRA='-DMICROPY_PY_THREAD=1'
+make ${MAKEOPTS} -C ports/stm32 BOARD=STM32L476DISC
+make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_WB55
+make ${MAKEOPTS} -C ports/stm32/mboot BOARD=PYBV10 CFLAGS_EXTRA='-DMBOOT_FSLOAD=1 -DMBOOT_VFS_LFS2=1'
+make ${MAKEOPTS} -C ports/stm32/mboot BOARD=PYBD_SF6
+make ${MAKEOPTS} -C ports/stm32/mboot BOARD=NUCLEO_WB55
+
+make ${MAKEOPTS} -C ports/nrf submodules
+make ${MAKEOPTS} -C ports/nrf BOARD=pca10040
+make ${MAKEOPTS} -C ports/nrf BOARD=microbit
+make ${MAKEOPTS} -C ports/nrf BOARD=pca10056 SD=s140
+make ${MAKEOPTS} -C ports/nrf BOARD=pca10090
+
+make ${MAKEOPTS} -C ports/bare-arm
+make ${MAKEOPTS} -C ports/minimal CROSS=1 build/firmware.bin
